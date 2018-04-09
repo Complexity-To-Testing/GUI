1a75ee030281aad899420cad8d64b6d95264e2d5
cristhian rodriguez <cristhian@footuro.es>
2018-04-10 01:00:19 +0200

    Mod preprocesar.sh

--- config.js
+++ config.js
@@ -1,15 +1,17 @@
 module.exports = {
- /*local
+ /*local*/
 dbName: "dbtfg",
 dbUser: "root",
 dbPassword: "",
 dbHost: "localhost",
-*/
+
 /* production*/
+/*
 dbName: "dbtfg",
 dbUser: "root",
 dbPassword: "root",
 dbHost: "localhost",
+*/
  // Puerto en el que escucha nuestra app.
  port: '5555'
 };
