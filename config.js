dcc2aaa5721c6e5478f07edcdbcbbf3de9f927d5
cristhian rodriguez <cristhian@footuro.es>
2018-04-10 02:51:02 +0200

    MOD host db

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ dbHost: "localhost",
 dbName: "dbtfg",
 dbUser: "root",
 dbPassword: "root",
-dbHost: "localhost",
+dbHost: "52.53.239.37",
 
  // Puerto en el que escucha nuestra app.
  port: '5555'
